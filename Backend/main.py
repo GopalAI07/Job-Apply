c3ac2789f2f4b1ec8472bb8261bda7c7d55f1a29
Gopal@02 <gopalchauhan.gc17@gmail.com>
2026-07-14 15:18:19 +0530

    Connect vercel with Bcakend

--- Backend/main.py
+++ Backend/main.py
@@ -21,7 +21,11 @@ def startup():
 # Allow the React dev server to call the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://job-apply-amedd38ur-gopalai07s-projects.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
